de19490701cc05987ca7dc371b1d2fae10831c69
Jesse Carter <jctr073@gmail.com>
2015-03-14 07:07:47 -0700

    adding login

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,16 @@
 module SessionsHelper
+  # Logs in the given user.
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+
+  # Returns the current logged-in user (if any).
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  # Return true if user id found with session id
+  def logged_in?
+    !current_user.nil?
+  end
 end
