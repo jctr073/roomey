4d3f6e28bb8ba42c65e606a1a071a4ef90a62067
davis-xie <davis.gh.xie@gmail.com>
2015-03-26 20:59:06 -0700

    add more details to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150326021757) do
+ActiveRecord::Schema.define(version: 20150327023948) do
 
   create_table "rooms", force: :cascade do |t|
     t.string   "neighborhood"
@@ -37,6 +37,9 @@ ActiveRecord::Schema.define(version: 20150326021757) do
     t.datetime "activated_at"
     t.string   "reset_digest"
     t.datetime "reset_sent_at"
+    t.integer  "age"
+    t.boolean  "seeker"
+    t.string   "gender"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
