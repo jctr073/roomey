e334c71c4392e60d899b84380e9bb4d4d5e89c42
Jesse Carter <jctr073@gmail.com>
2015-03-28 15:36:44 -0700

    Adding amenities to room rental view

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -4,8 +4,13 @@ class RoomsController < ApplicationController
   end
 
   def create
+    
+    sel_amenities = params[:room][:amenities]
     @room = current_user.rooms.build(room_params)
     if @room.save
+      sel_amenities.each do |id|
+        @room.amenities << Amenity.find(id.to_i) unless id.empty?
+      end
       flash[:success] = "Room Posted"
       redirect_to room_path(@room.id)
     else
@@ -26,8 +31,8 @@ class RoomsController < ApplicationController
 
   private
     def room_params
-      params.require(:room).permit(:short_desc, 
-                                   :price, :description, :size, 
+      params.require(:room).permit(:short_desc, :price, 
+                                   :description, :size, 
                                    :neighborhood_id)
     end
 end
