045489efa25cb9955f8ca974baab237dd0d41895
Jesse Carter <jctr073@gmail.com>
2015-03-28 08:22:14 -0700

    adding ablitity to create new room rentals

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,5 +1,16 @@
 class RoomsController < ApplicationController
   def new
+    @room = current_user.rooms.build
+  end
+
+  def create
+    @room = current_user.rooms.build(room_params)
+    if @room.save
+      flash[:success] = "Room Posted"
+      redirect_to room_path(@room.id)
+    else
+      render 'new'
+    end
   end
 
   def edit
@@ -12,4 +23,11 @@ class RoomsController < ApplicationController
   def index
   	@rooms = Room.paginate(page: params[:page])
   end
+
+  private
+    def room_params
+      params.require(:room).permit(:short_desc, 
+                                   :price, :description, :size, 
+                                   :neighborhood_id)
+    end
 end
