5a12f36e556fec652ad0423ee737d24bf30e9655
davis-xie <davis.gh.xie@gmail.com>
2015-03-30 20:08:01 -0700

    seeker default false

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      @user.send_activation_email
+       @user.send_activation_email
       flash[:info] = "Please check your email to activate your account"
       redirect_to root_url
     else
@@ -24,6 +24,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+     
   end
 
   def edit
@@ -49,7 +50,7 @@ class UsersController < ApplicationController
   private
     def user_params
       params.require(:user).permit(:name, :email,
-                                   :password, :password_confirmation,:age,:gender)
+                                   :password, :password_confirmation,:age,:gender,:seeker)
     end
 
   # Before filters
