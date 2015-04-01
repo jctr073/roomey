7bd5d4817b1d2e78ab71324a39b9cc6f4fdd60b0
Jesse Carter <jctr073@gmail.com>
2015-03-31 23:15:29 -0700

    Fix migration and seed files

--- db/migrate/20150327023948_add_details_to_users.rb
+++ db/migrate/20150327023948_add_details_to_users.rb
@@ -1,7 +1,7 @@
 class AddDetailsToUsers < ActiveRecord::Migration
   def change
     add_column :users, :age, :interger
-    add_column :users, :seeker, :boolean default: false
+    add_column :users, :seeker, :boolean, default: false
     add_column :users, :gender, :string
   end
 end
